d56d4ca3e615bd7170ceaff46634e296043dfe91
desiatcodaniel <desiatcodaniel@gmail.com>
2022-05-22 22:35:53 -0700

    initial setup with Material UI

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import image from './images/dotalogo.png'
 
 function App() {
   return (
-    <div className="App">
+    <div style={{fontFamily:"roboto"}}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={image} style={{width:"100px"}} alt="logo" />
       </header>
     </div>
   );
